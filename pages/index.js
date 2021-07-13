059778a30a835959e6c306a145d9d079a19a6070
João Albuquerque <joaopasantos0712@gmail.com>
2021-07-12 22:33:39 -0300

    Progresso da Aula 01

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(props) {
+  return (
+    <Box>
+      <img
+        src={`https://github.com/${props.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+    </Box>
+  );
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = "joaopasantos";
+  const friends = ["juunegreiros", "omariosouto", "peas", "rafaballerini"];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div style={{ gridArea: "profileArea" }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div style={{ gridArea: "welcomeArea" }}>
+          <Box>
+            <h1 className="title">Bem-vindo(a)</h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div style={{ gridArea: "profileRelationsArea" }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Amigos ({friends.length})</h2>
+            <ul>
+              {friends.map((current) => {
+                return (
+                  <li>
+                    <a href={`/users/${current}`} key={current}>
+                      <img src={`https://github.com/${current}.png`} />
+                      <span>{current}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </>
+  );
 }
