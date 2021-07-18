2b9bc4b9a5e80e7b31e104997afb362c4f6a839b
João Albuquerque <joaopasantos0712@gmail.com>
2021-07-18 13:02:52 -0300

    Correção na API auth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -3,23 +3,18 @@ import jwt from 'jsonwebtoken';
 export default async function requestHandler(request, response) {
   const { authorization } = request.headers;
   const token = authorization;
+
   const { githubUser } = jwt.decode(token);
   await fetch(`https://api.github.com/users/${githubUser}`)
     .then((res) => res.json())
     .then((res) => {
       if (res.message === 'Not Found') {
-        response.json({
+        response.send({
           isAuthenticated: false,
         });
-
-        return;
       }
-      response.json({
+      response.send({
         isAuthenticated: true,
       });
     });
-
-  response.status(404).json({
-    message: 'Rota GET não implementada.',
-  });
 }
