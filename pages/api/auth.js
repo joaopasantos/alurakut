c8df50947da75b9b0c3188ab26ec47ed16d249cb
João Albuquerque <joaopasantos0712@gmail.com>
2021-07-18 13:09:01 -0300

    Correção na API auth

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 export default async function requestHandler(request, response) {
-  const { authorization } = request.headers;
-  const token = authorization;
+  const { Authorization } = request.headers;
+  const token = Authorization;
 
   const { githubUser } = jwt.decode(token);
-  await fetch(`https://api.github.com/users/${githubUser}`)
+  fetch(`https://api.github.com/users/${githubUser}`)
     .then((res) => res.json())
     .then((res) => {
       if (res.message === 'Not Found') {
